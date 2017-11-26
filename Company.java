313d364285a00827834ea8b4584b54d88558e7d8
Kaitlyn <koura@email.arizona.edu>
2017-11-26 15:49:39 -0700

    company class for system

---
+++ Company.java
@@ -0,0 +1,12 @@
+package system.company;
+
+import system.people.*;
+
+import java.util.ArrayList;
+
+import system.hardware.*;
+
+public class Company {
+	ArrayList<Person> people;
+	ArrayList<Equipment> equipmentList;
+}
